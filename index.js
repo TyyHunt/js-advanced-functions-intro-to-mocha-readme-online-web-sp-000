141498789b2feec4c2c86bb1433e5eccb50289ff
Tyler Hunt <tyhunt04@gmail.com>
2019-06-15 19:12:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,11 +10,17 @@ function sayHeyFriend(friend) {
   return `Hey, ${friend}!`
 }
 
-function favoriteIceCream() {
-  let NAME_OF_ICECREAM = 'mint chocolate chip'
+function favoriteIceCream(NAME_OF_ICECREAM) {
+  
   return `I love ${NAME_OF_ICECREAM}`
 }
 
 function shouting(string) {
   return string.toUpperCase()
 }
+
+function roundDown(num) {
+  return num.floor
+}
+
+
